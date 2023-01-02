48208b68635c69a544d8cbc78b230c8922244931
Madebybrown <madebybrown@gmail.com>
2023-01-02 14:50:18 +0000

    Add filter sets to comments and profiles

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
-from socials_drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from socials.drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
@@ -8,6 +9,8 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
